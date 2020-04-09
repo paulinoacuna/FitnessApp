38a50ac096c255fce337413323735d878cc7e648
Paulino Acuna <paulinomaven911@gmail.com>
2020-04-09 18:34:30 -0500

    imagenes agregadas

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,22 @@
 import React from "react";
+import img_1 from "../images/exercise.png"
 //Componente de tipo Clase
 
 class Card extends React.Component {
-    //FUNCION OBLIGATORIA
-    render(){
-        return 
+    //FUNCION OBLIGATORIA RENDER
+    render() {
+        return (
+            <div>
+                <div>
+                    <img src={img_1} />
+                </div>
+
+                <div>
+                    <h1>Technique Guides</h1>
+                    <p>Learn amazing street workout and calisthenics</p>
+                </div>
+            </div>
+        )
     }
 }
 
