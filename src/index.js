c81ac861880e9e5f69e929f11379a40b1c83dccc
Paulino Acuna <paulinomaven911@gmail.com>
2020-04-09 17:52:32 -0500

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/**Autor: Paulino Acuña
+ * Curso de React.js
+ * 
+ * index.js es el archivo principal que se conecta con el index.html, 
+ * que tiene como elemento un div con id = root
+ */
+
+//FORMA ANTIGUA DE TRABAJAR
+
+//const element = document.createElement("h1");         //crea un elemento para ser usado en html de tipo h1
+//element.innerText = "Hello React..";                  //inyecta un texto al elemento
+//const container = document.getElementById("root");    //crea contenedor y lo asocia al div con id "root" del html
+//container.appendChild(element);                       //agrega un hijo al contenedor
+
+
+//FORMA DE CREARLO CON REACT!
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+//objeto user
+const user = {
+    firstName: "Raul",
+    lastName: "Palacios",
+    avatar: "http://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png"
+}
+//metodos
+function getName(user) {
+    return `${user.firstName} ${user.lastName}`
+}
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello {getName(user)} </h1>
+    }
+    return <h1>Hello Stranger</h1>
+}
+
+
+
+
+
+//MAIN
+const element = (
+    <div>
+        <h1>{getGreeting(user)}</h1>
+        <img src={user.avatar} />
+    </div>
+)
+
+
+//FINAL RENDER EXPORTATION
+const container = document.getElementById("root")
+ReactDOM.render(element, container)                   //(que renderear, donde renderear)
