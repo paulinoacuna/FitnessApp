552081d372de486d471daaeb1d758b06d5cf3f99
Paulino Acuna <paulinomaven911@gmail.com>
2020-04-10 20:52:02 -0500

    usando states

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,30 @@ import "./styles/Card.css"
 
 //Componente de tipo Clase
 class Card extends React.Component {
-    //FUNCION OBLIGATORIA RENDER
+
+    //definir constructor pasando props al padre
+    constructor(props) {
+        super(props)
+        //default state
+        this.state = {
+            image: "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc"
+        }
+    }
+
+
+    
+    componentDidMount() { //se invoca inmediatamente despues de que un componente es montado
+        setTimeout(() => {
+            //aqui se modifica el estado del componente a los 5 seg
+            this.setState({
+                image: "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+            })
+
+        }, 5000)
+    }
+
     render() {
         const { title, description, img, leftColor, rightColor } = this.props
-
-
         return (
             <div className="card mx-auto Fitness-Card"
                 //ESTILOS EN LINEA: ABRE LA CAPACIDAD DE INYECTAR VARIABLES EN LOS ESTILOS
@@ -23,7 +42,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={img} className="float-right" />
+                            <img src={this.state.image} className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
