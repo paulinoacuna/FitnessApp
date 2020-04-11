86714a993300ebf16060d3252f66bb151e34a1dc
Paulino Acuna <paulinomaven911@gmail.com>
2020-04-10 21:29:14 -0500

    componentes funcionales

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ import ReactDOM from "react-dom";
 import Card from "./components/Card"  //importamos el componente Card
 //importamos bootstrap
 import "bootstrap/dist/css/bootstrap.css"
+import Welcome from "./components/Welcome";
 /**
  * //objeto user
 const user = {
@@ -54,12 +55,18 @@ function getGreeting(user) {
 
 //FINAL RENDER EXPORTATION
 const container = document.getElementById("root")
-ReactDOM.render(<Card
-    //a continuacion defino el elemento card con sus atributos y estilos para que el componente los reciba
-    title="Technique Guides"
-    description="Learn amazing street workout and calisthenics skills"
-    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-    leftColor="#A74CF2"
-    rightColor="#617BFB"
+ReactDOM.render(
+    <div>
+        <Welcome 
+            username="Paulino"
+        />
+        <Card
+            //a continuacion defino el elemento card con sus atributos y estilos para que el componente los reciba
+            title="Technique Guides"
+            description="Learn amazing street workout and calisthenics skills"
+            img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+            leftColor="#A74CF2"
+            rightColor="#617BFB"
+        />
 
-/>, container)
+    </div>, container)
